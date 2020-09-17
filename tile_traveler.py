48082f72346ef0bb4f0aad86877b5930f8e36618
arnort20 <arnort20@ru.is>
2020-09-17 10:01:59 +0000

    vbyhewqa

--- tile_traveler.py
+++ tile_traveler.py
@@ -1,7 +1,13 @@
 X = 1
 y = 1
 while True:
-    
+    #hér kemur "You can travel"
+    direction = str(input("Direction: "))
+    if (direction == 'n') or (direction == 'N'):
+        
+
+
+
 """
 You can travel:
 """
